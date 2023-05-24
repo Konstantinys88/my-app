a991d0e0a930cd6cf4c3ab4e57b6b502a6b2920e
Konstantin0 <maschkovc@yandex.ru>
2023-05-24 14:09:33 +0300

    init

---
+++ src/components/app-info-header/app-header.js
@@ -0,0 +1,16 @@
+import "./app-header.css";
+
+const AppHeader = () => {
+    return (
+        <div className="app-header">
+            <ul className="app-header-ul">
+                <li>Logo</li>
+                <li>Пункт 1</li>
+                <li>Пункт 2</li>
+                <li>Пункт 3</li>
+            </ul>
+        </div>
+    )  
+}
+
+export default AppHeader;
---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import AppHeader from "../app-info-header/app-header";
+
+import "./app.css";
+
+function App() {
+    return(
+        <div className="app">
+            <AppHeader></AppHeader>
+        </div>
+    );
+}
+
+export default App;
